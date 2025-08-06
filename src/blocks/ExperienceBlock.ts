2ba8a3f428d3d36fa78e49e9dd9cf32b2dd36bc8
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-08-06 11:33:48 +0200

    Update ExperienceBlock.ts

--- src/blocks/ExperienceBlock.ts
+++ src/blocks/ExperienceBlock.ts
@@ -10,7 +10,7 @@ import { ProjectBlock } from "./ProjectBlock";
 import { HighlightDecorator } from "../decorators/HighlightDecorator";
 
 export class ExperienceBlock implements IBlock {
-  constructor(private d: Experience) {}
+  constructor(private d: Experience[]) {}
 
   /**
    * Рендеринг блоку досвіду роботи
@@ -24,37 +24,25 @@ export class ExperienceBlock implements IBlock {
     container.className = "section experience";
     container.innerHTML = "<h2>Experience</h2>";
 
-    // TODO: Для кожного досвіду створити div.experience-item з innerHTML (позиція, компанія, період)
-    this.d.experienceItems.forEach((item) => {
+    // Для кожного досвіду створити div.experience-item з позицією, компанією та періодом
+    this.d.forEach((item) => {
       const div = document.createElement("div");
       div.className = "experience-item";
       div.innerHTML = `
         <h3>${item.position}</h3>
-        <p><em>${item.company}</em> (${item.period})</p>
-        <p>${item.description}</p>
+        <p><em>${item.company}</em> (${item.start} – ${item.end})</p>
       `;
       // Для кожного проекту створюємо блок і, за потреби, обгортаємо декоратором
       item.projects.forEach((proj: Project) => {
-        const block = new ProjectBlock(proj);
-        // якщо recent — обгорнемо декоратором
-        const element = proj.isRecent
-          ? new HighlightDecorator(block).render()
-          : block.render();
-        div.appendChild(element);
+        let block: IBlock = new ProjectBlock(proj);
+        if (proj.isRecent) {
+          block = new HighlightDecorator(block);
+        }
+        div.appendChild(block.render());
       });
       container.appendChild(div);
     });
 
     return container;
   }
-}
-
-export interface Experience {
-  experienceItems: {
-    position: string;
-    company: string;
-    period: string;
-    description: string;
-    projects: Project[];
-  }[];
 }
