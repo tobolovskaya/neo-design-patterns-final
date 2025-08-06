cb68becaf2733d008e37a173f0d08b74a4ac4d37
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-08-06 11:32:14 +0200

    Update EducationBlock.ts

--- src/blocks/EducationBlock.ts
+++ src/blocks/EducationBlock.ts
@@ -6,7 +6,7 @@ import { Education } from "../models/ResumeModel";
 import { IBlock } from "./BlockFactory";
 
 export class EducationBlock implements IBlock {
-  constructor(private d: Education) {}
+  constructor(private d: Education[]) {}
 
   /**
    * Рендеринг блоку освіти
@@ -19,11 +19,10 @@ export class EducationBlock implements IBlock {
     el.className = "section education";
     el.innerHTML = "<h2>Education</h2>";
 
-    // TODO: Додайте до секції інформацію про навчальний заклад (this.d.institution),
-    // отриманий ступінь (this.d.degree) та період навчання (this.d.graduation)
-    this.d.forEach(e => {
-      const div = document.createElement('div');
-      div.className = 'education-item';
+    /// Додайте до секції інформацію про навчальний заклад, ступінь та рік випуску
+    this.d.forEach((e) => {
+      const div = document.createElement("div");
+      div.className = "education-item";
       div.innerHTML = `${e.degree} ${e.field}, ${e.institution} (${e.graduation})`;
       el.appendChild(div);
     });
