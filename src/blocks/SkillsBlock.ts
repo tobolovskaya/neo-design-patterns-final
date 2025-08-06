592597a196add40a707e915fdf0ca2d4060e3fc1
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-08-06 10:32:59 +0200

    Update SkillsBlock.ts

--- src/blocks/SkillsBlock.ts
+++ src/blocks/SkillsBlock.ts
@@ -21,9 +21,17 @@ export class SkillsBlock implements IBlock {
     sec.innerHTML = "<h2>Skills</h2>";
 
     // TODO: Створіть список навичок, згрупований за категоріями
+    const ul = document.createElement('ul');
+    ul.className = 'skills-list';
     // Використайте Object.entries(this.d) для ітерації по всіх категоріях
     // Створіть для кожної категорії елемент списку з назвою категорії та списком навичок
-
+    Object.entries(this.d).forEach(([category, skills]) => {
+      const li = document.createElement('li');
+      li.className = 'skills-category';
+      li.innerHTML = `<strong>${category}</strong>: ${skills.join(', ')}`;
+      ul.appendChild(li);
+    });
+    sec.appendChild(ul);
     return sec;
   }
 }
